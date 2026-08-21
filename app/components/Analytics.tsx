c2fe1c5366c456bfa79c0b888cb605933441671c
Joel David <149636076+montillaJoel@users.noreply.github.com>
2026-08-21 09:46:12 -0400

    Analisis de usuaio

---
+++ app/components/Analytics.tsx
@@ -0,0 +1,10 @@
+// export default function Analytics() {
+//     return (
+//         <script
+//             async
+//             defer
+//             data-website-id="f3e5c0d1-4b6a-4c8e-9f2b-1a2b3c4d5e6f"
+//             src="https://analytics.example.com/umami.js"
+//         ></script>
+//     );
+// }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import {FC, PropsWithChildren}  from "react";
 import "@/app/ui/globals.css"
-
+import { Analytics } from "@vercel/analytics/next";
 const RootLayout: FC<PropsWithChildren> = ({children}) =>{
     return (
         <html>
             <body>
                 {children}
+                <Analytics />
             </body>
         </html>
     );
